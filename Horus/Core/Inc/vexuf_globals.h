f0a974b4a1deffd1fa5d1a0be88a8b176a4ca287
Aly Badawy <AlyBadawy@users.noreply.github.com>
2024-07-22 16:56:31 -0400

    Some globals

---
+++ Horus/Core/Inc/vexuf_globals.h
@@ -0,0 +1,38 @@
+/*
+ * vexuf_globals.h
+ *
+ *  Created on: Jul 22, 2024
+ *      Author: Aly Badawy
+ */
+
+#ifndef INC_VEXUF_GLOBALS_H_
+#define INC_VEXUF_GLOBALS_H_
+
+#define HW_NAME "Horus"
+#define HW_REV "5.7.3"
+#define HW_DATE "2430"
+
+#define FW_VERSION "0.2.0"
+
+
+#define EEPROM_SERIAL_NUMBER_ADDR     0x0000
+#define EEPROM_CALLSIGN_ADDR          0x000A
+#define EEPROM_RESERVED_ADDR          0x0014
+#define EEPROM_V1_ENABLED_ADDR        0x0018
+#define EEPROM_V2_ENABLED_ADDR        0x0019
+#define EEPROM_V3_ENABLED_ADDR        0x001A
+#define EEPROM_V1_LED_ENABLED_ADDR    0x001B
+#define EEPROM_V2_LED_ENABLED_ADDR    0x001C
+#define EEPROM_V3_LED_ENABLED_ADDR    0x001D
+#define EEPROM_SERVO1_ENABLED_ADDR    0x001E
+#define EEPROM_SERVO2_ENABLED_ADDR    0x001F
+#define EEPROM_SPI_ENABLED_ADDR       0x0020
+#define EEPROM_I2C_ENABLED_ADDR       0x0021
+#define EEPROM_ACTUATORS_ENABLED_ADDR 0x0022
+#define EEPROM_TTL_ENABLED_ADDR       0x002A
+#define EEPROM_TNC_ENABLED_ADDR       0x002B
+#define EEPROM_BUZZER_ENABLED_ADDR    0x002C
+
+
+
+#endif /* INC_VEXUF_GLOBALS_H_ */
